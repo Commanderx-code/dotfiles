add6e5d70ec2c623f0c340a3a8dcfc4ecde7307b
Commanderx-code <dirtyprodigy@protonmail.com>
2026-06-03 02:06:01 -0400

    Manage shell tools with Home Manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -18,16 +18,54 @@
     btop
     fastfetch
     lazygit
+    chafa
+    jq
+    unzip
+    wget
+    curl
   ];
 
   programs.git = {
-  enable = true;
-  settings = {
-    user.name = "Commanderx-code";
-    user.email = "dirtyprodigy@protonmail.com";
+    enable = true;
+    settings = {
+      user.name = "Matthew March";
+      user.email = "dirtyprodigy@protonmail.com";
+      init.defaultBranch = "main";
+      pull.rebase = false;
+    };
   };
-};
 
-  programs.fish.enable = true;
-  programs.starship.enable = true;
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.fish = {
+    enable = true;
+
+    shellAliases = {
+      ll = "eza -lah --icons";
+      la = "eza -la --icons";
+      lt = "eza --tree --level=2 --icons";
+      cat = "bat";
+      grep = "rg";
+      gs = "git status";
+      ga = "git add";
+      gc = "git commit";
+      gp = "git push";
+      gl = "git pull";
+      lg = "lazygit";
+      update = "sudo pacman -Syu && paru -Sua";
+      hms = "home-manager switch";
+    };
+
+    interactiveShellInit = ''
+      zoxide init fish | source
+      fzf --fish | source
+    '';
+  };
+
+  programs.fastfetch = {
+    enable = true;
+  };
 }
